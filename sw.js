5975df2595c9ace85b5adc39831927cf2997eabf
Your Name <you@example.com>
2026-01-08 07:45:33 +0000

    commit message

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "gfa-ref-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/Ghana-Football-Association.jpg",
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
